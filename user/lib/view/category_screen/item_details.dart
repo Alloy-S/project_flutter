26090d79b8ec23b1a9f24d685f7e1f66770186ac
C14210224 <107255810+C14210224@users.noreply.github.com>
2024-06-12 20:40:50 +0700

    fixed overflow

--- user/lib/view/category_screen/item_details.dart
+++ user/lib/view/category_screen/item_details.dart
@@ -3,7 +3,7 @@ import 'package:emart_app/consts/consts.dart';
 import 'package:emart_app/widgets_common/our_button.dart';
 import 'package:flutter/material.dart';
 
-class ItemDetails extends StatelessWidget{
+class ItemDetails extends StatelessWidget {
   final String? title;
   const ItemDetails({Key? key, required this.title}) : super(key: key);
 
@@ -14,59 +14,68 @@ class ItemDetails extends StatelessWidget{
       appBar: AppBar(
         title: title!.text.color(darkFontGrey).fontFamily(bold).make(),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-           IconButton(onPressed: (){}, icon: Icon(Icons.favorite_outline)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.share)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_outline)),
         ],
       ),
       body: Column(
         children: [
           Expanded(
             child: Padding(
-            padding:  const EdgeInsets.all(8),
+              padding: const EdgeInsets.all(8),
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    //swiper section
+                    // swiper section
                     VxSwiper.builder(
                       autoPlay: true,
                       height: 350,
                       itemCount: 3,
-                      aspectRatio: 16 / 9, itemBuilder: (context, index){
-                      return Image.asset(imgFc5, width: double.infinity, fit: BoxFit.cover);
-                    }),
-
+                      aspectRatio: 16 / 9,
+                      itemBuilder: (context, index) {
+                        return Image.asset(imgFc5, width: double.infinity, fit: BoxFit.cover);
+                      },
+                    ),
                     10.heightBox,
-                    //title dan detail 
+                    // title and detail
                     title!.text.size(16).color(darkFontGrey).fontFamily(semibold).make(),
                     10.heightBox,
-                    //rating
-                    VxRating(onRatingUpdate: (value){}, normalColor: textfieldGrey, selectionColor: golden, maxRating: 5, size: 25, stepInt: true),
-                    
+                    // rating
+                    Container(
+                      width: MediaQuery.of(context).size.width, // Adjust the width to avoid overflow
+                      child: VxRating(
+                        onRatingUpdate: (value) {},
+                        normalColor: textfieldGrey,
+                        selectionColor: golden,
+                        maxRating: 5,
+                        size: 25,
+                        stepInt: true,
+                      ),
+                    ),
                     10.heightBox,
                     "\$30,000".text.color(redColor).fontFamily(bold).size(18).make(),
-
                     10.heightBox,
-                    
                     Row(
                       children: [
-                        Expanded(child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            "Seller".text.white.fontFamily(semibold).make(),
-                            5.heightBox,
-                            "In House Brands".text.fontFamily(semibold).color(darkFontGrey).size(16).make(),
-                          ],
-                        )),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              "Seller".text.white.fontFamily(semibold).make(),
+                              5.heightBox,
+                              "In House Brands".text.fontFamily(semibold).color(darkFontGrey).size(16).make(),
+                            ],
+                          ),
+                        ),
                         CircleAvatar(
                           backgroundColor: Colors.white,
                           child: Icon(Icons.message_rounded, color: darkFontGrey),
                         )
                       ],
                     ).box.height(60).padding(EdgeInsets.symmetric(horizontal: 16)).color(textfieldGrey).make(),
-
-                    //color section
+                    // color section
                     20.heightBox,
                     Column(
                       children: [
@@ -81,9 +90,7 @@ class ItemDetails extends StatelessWidget{
                             ),
                           ],
                         ).box.padding(const EdgeInsets.all(8)).make(),
-
-
-                        //total row
+                        // total row
                         Row(
                           children: [
                             SizedBox(
@@ -92,7 +99,7 @@ class ItemDetails extends StatelessWidget{
                             ),
                             Row(
                               children: [
-                                IconButton(onPressed: (){}, icon: const Icon(Icons.remove)),
+                                IconButton(onPressed: () {}, icon: const Icon(Icons.remove)),
                                 "0".text.size(16).color(redColor).fontFamily(bold).make(),
                                 IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
                                 10.widthBox,
@@ -101,7 +108,6 @@ class ItemDetails extends StatelessWidget{
                             ),
                           ],
                         ).box.padding(const EdgeInsets.all(8)).make(),
-                        
                         Row(
                           children: [
                             SizedBox(
@@ -110,7 +116,7 @@ class ItemDetails extends StatelessWidget{
                             ),
                             Row(
                               children: [
-                                IconButton(onPressed: (){}, icon: const Icon(Icons.remove)),
+                                IconButton(onPressed: () {}, icon: const Icon(Icons.remove)),
                                 "0".text.size(16).color(darkFontGrey).fontFamily(bold).make(),
                                 IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
                                 10.widthBox,
@@ -119,70 +125,57 @@ class ItemDetails extends StatelessWidget{
                             ),
                           ],
                         ).box.padding(const EdgeInsets.all(8)).make(),
-
-
                       ],
                     ).box.white.shadowSm.make(),
-
-                    //deskripsi
+                    // description
                     10.heightBox,
-
                     "Description".text.color(darkFontGrey).fontFamily(semibold).make(),
-
                     10.heightBox,
                     "This is a dummy item and dummy description here..".text.color(darkFontGrey).make(),
-
                     10.heightBox,
-
                     ListView(
                       physics: BouncingScrollPhysics(),
                       shrinkWrap: true,
-                      children: List.generate(ItemDetailsButtonList.length, (index)=> ListTile(
+                      children: List.generate(ItemDetailsButtonList.length, (index) => ListTile(
                         title: ItemDetailsButtonList[index].text.fontFamily(semibold).color(darkFontGrey).make(),
                         trailing: const Icon(Icons.arrow_forward),
                       )),
                     ),
-
                     20.heightBox,
-                    //product may like 
-
+                    // products you may like
                     productsyoumaylike.text.fontFamily(bold).size(16).color(darkFontGrey).make(),
-
                     10.heightBox,
                     SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-
-                        child: Row(children: List.generate(6, (index) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(6, (index) => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Image.asset(imgP1, width: 150, fit: BoxFit.cover,
-                            ),
+                            Image.asset(imgP1, width: 150, fit: BoxFit.cover),
                             10.heightBox,
                             "Laptop 4GB/64GB".text.fontFamily(semibold).color(darkFontGrey).make(),
                             10.heightBox,
                             "\$600".text.color(redColor).fontFamily(bold).size(16).make(),
-                            10.heightBox, 
+                            10.heightBox,
                           ],
                         ).box.white.margin(EdgeInsets.symmetric(horizontal: 4)).roundedSM.padding(const EdgeInsets.all(8)).make()),
-                                      
-                        ),
                       ),
-
+                    ),
                   ],
-                  ),
+                ),
               ),
-              )),
+            ),
+          ),
           SizedBox(
             width: double.infinity,
             height: 60,
             child: ourButton(
               color: redColor,
-              onPress: (){},
+              onPress: () {},
               textColor: whiteColor,
-              title: "Add to cart"
+              title: "Add to cart",
             ),
-          )
-
+          ),
         ],
       ),
     );
